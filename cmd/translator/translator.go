36f1ab7aa02d0bc085c92d289ad6a5a8511401ce
Moleus <fafufuburr@gmail.com>
2023-11-29 21:51:16 +0300

    feat(translator): implement machine code translation and encoding

--- cmd/translator/translator.go
+++ cmd/translator/translator.go
@@ -65,11 +65,15 @@ START:
 package translator
 
 import (
-	"bytes"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 var (
@@ -79,7 +83,7 @@ var (
 )
 
 type Translator interface {
-	Translate(input io.Reader, output io.Writer) error
+	Translate(input string) (string, error)
 }
 
 type translator struct {
@@ -90,28 +94,160 @@ func NewTranslator() Translator {
 	return &translator{}
 }
 
-func (t *translator) ParseTerms(input io.Reader) ([]string, error) {
-	// TODO
-	terms := []string{}
-	return terms, nil
+// ParsedInstruction
+// Каждая инструкция находится на новой строке.
+// Метки должны находиться на той же строке, что и инструкции
+// возможный вариант строки с инструкцией:
+// <label>: <instruction> <operand>
+// <instruction> <operand>
+type ParsedInstruction struct {
+	label       string
+	instruction string
+	operand     string
+	metaInfo    TermMetaInfo
 }
 
-func (t *translator) ConvertTermsToMachineCode(terms []string) (string, error) {
-	// TODO
-	return "", nil
+// ParsedConstant - объявленная константа в исходном коде
+type ParsedConstant struct {
+	label string
+	value int
 }
 
-func (t *translator) Translate(input io.Reader, output io.Writer) error {
-	terms, err := t.ParseTerms(input)
-	if err != nil {
-		return err
+type ParseError struct {
+	content string
+	line    int
+}
+
+func (e ParseError) Error() string {
+	return fmt.Sprintf("invalid instruction at %d: %s", e.line, e.content)
+}
+
+func NewParseError(content string, line int) error {
+	return ParseError{content, line}
+}
+
+func ParseInstruction(line string, lineNumber int) (ParsedInstruction, error) {
+	// group2: label, group3: instruction, group4: operand
+	instructionRegexTmpl := `^((\w+)?\s*:)?\s*(\w+)\s*(\w+)$`
+	instructionRegex := regexp.MustCompile(instructionRegexTmpl)
+	matches := instructionRegex.FindStringSubmatch(line)
+	if len(matches) == 0 {
+		return ParsedInstruction{}, NewParseError(line, lineNumber)
 	}
-	machineCode, err := t.ConvertTermsToMachineCode(terms)
-	if err != nil {
-		return err
+	// TODO: debug matches
+	instruction := ParsedInstruction{
+		label:       matches[2],
+		instruction: matches[3],
+		operand:     matches[4],
+		metaInfo: TermMetaInfo{
+			LineNum:         lineNumber,
+			OriginalContent: line,
+		},
 	}
-	output.Write(bytes.NewBufferString(machineCode).Bytes())
-	return nil
+	return instruction, nil
+}
+
+// ParseConstant takes a line and returns a constant if any
+// Constant input is represented as
+// CONST <label>: <value>
+func ParseConstant(line string, lineNum int) (ParsedConstant, error) {
+	constantRegexTmpl := `^CONST\s+(\w+)\s*:\s*(\d+)$`
+	constantRegex := regexp.MustCompile(constantRegexTmpl)
+	matches := constantRegex.FindStringSubmatch(line)
+	if len(matches) == 0 {
+		return ParsedConstant{}, NewParseError(line, lineNum)
+	}
+	value, err := strconv.Atoi(matches[2])
+	if err != nil {
+		return ParsedConstant{}, NewParseError(line, lineNum)
+	}
+	return ParsedConstant{
+		label: matches[1],
+		value: value,
+	}, nil
+}
+
+func (t *translator) ParseConstants(input string) ([]ParsedConstant, error) {
+	var constants []ParsedConstant
+
+	lines := strings.Split(input, "\n")
+	for i, line := range lines {
+		if !strings.HasPrefix(line, "CONST") {
+			continue
+		}
+		constant, err := ParseConstant(line, i+1)
+		if err != nil {
+			return nil, err
+		}
+		constants = append(constants, constant)
+	}
+	return constants, nil
+}
+
+func (t *translator) ParseInstructions(input string) ([]ParsedInstruction, error) {
+	var instructions []ParsedInstruction
+
+	lines := strings.Split(input, "\n")
+	for i, line := range lines {
+		instruction, err := ParseInstruction(line, i+1)
+		if err != nil {
+			return nil, err
+		}
+		instructions = append(instructions, instruction)
+	}
+
+	return instructions, nil
+}
+
+type TermMetaInfo struct {
+	LineNum         int    `json:"line_num"`
+	OriginalContent string `json:"original_content"`
+}
+
+type MachineCodeTerm struct {
+	Index    int          `json:"index"`
+	Opcode   string       `json:"opcode"`
+	Operand  string       `json:"operand"`
+	TermInfo TermMetaInfo `json:"term_info"`
+}
+
+func (t *translator) ConvertTermsToMachineCode(instructions []ParsedInstruction) ([]MachineCodeTerm, error) {
+	var machineCode []MachineCodeTerm
+	for i, instruction := range instructions {
+		// TODO: it can be variable, not instruction
+		newMachineCodeTerm := MachineCodeTerm{
+			Index:    i,
+			Opcode:   instruction.instruction,
+			Operand:  instruction.operand,
+			TermInfo: instruction.metaInfo,
+		}
+		machineCode = append(machineCode, newMachineCodeTerm)
+	}
+	return machineCode, nil
+}
+
+func (t *translator) EncodeMachineCode(machineCode []MachineCodeTerm) (string, error) {
+	encodedCode, err := json.Marshal(machineCode)
+	if err != nil {
+		return "", err
+	}
+	return string(encodedCode), nil
+}
+
+func (t *translator) Translate(input string) (string, error) {
+	parsedInstructions, err := t.ParseInstructions(input)
+	if err != nil {
+		return "", err
+	}
+	machineCode, err := t.ConvertTermsToMachineCode(parsedInstructions)
+	if err != nil {
+		return "", err
+	}
+	encodedMachineCode, err := t.EncodeMachineCode(machineCode)
+	if err != nil {
+		return "", err
+	}
+	return encodedMachineCode, nil
 }
 
 /*
@@ -134,7 +270,12 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		defer f.Close()
+		defer func(f *os.File) {
+			err := f.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}(f)
 		input = f
 	}
 
@@ -145,12 +286,25 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		defer f.Close()
+		defer func(f *os.File) {
+			err := f.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}(f)
 		output = f
 	}
 
 	translator := NewTranslator()
-	err := translator.Translate(input, output)
+	inputStr, err := io.ReadAll(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result, err := translator.Translate(string(inputStr))
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = output.Write([]byte(result))
 	if err != nil {
 		log.Fatal(err)
 	}
